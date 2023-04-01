06d32f1875b4649e192e4f9c3b91ee83d77c91b7
fideleChester <fidele.eklou@ifnti.com>
2023-04-01 11:23:00 +0000

    Add about page and update links

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,13 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello City</title>
+    <title>About Us | Hello City</title>
 </head>
 <body>
-    <h1>Hello from Quebec </h1>
+    <p>Built with &hearts; by ELFID K@K@U.</p>
 
-    <p>It's currently {{date('h:i A')}}</p>
-
+    <p><a href="/">Revenir à  la page d'Accueil</a></p>
     <footer>
         <p>&copy; Copyright {{date('Y')}} &middot; <a href="/about-us" >About Us</a></p>
     </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
+    use Illuminate\Support\Facades\Route;
 
-use Illuminate\Support\Facades\Route;
+    Route::get('/',function(){
+        return view('home');
+    });
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('home');
-});
+    Route::get('/about-us',function(){
+        return view('about');
+    });
