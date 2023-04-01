3f952e14e01d9163ab8f443c065189319bbb2292
fideleChester <fidele.eklou@ifnti.com>
2023-04-01 10:54:05 +0000

    UPDATE readme and homePage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{str_replace('_','=',app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello City</title>
+</head>
+<body>
+    <h1>Hello from Quebec </h1>
+
+    <p>It's currently {{date('h:i A')}}</p>
+
+    <footer>
+        <p>&copy; Copyright {{date('Y')}} &middot; <a href="#" >About Us</a></p>
+    </footer>
+</body>
+</html>
