b9740bc0fee7809ccc9da25b7610c67b77d7521c
fideleChester <fidele.eklou@ifnti.com>
2023-04-01 12:09:13 +0000

    remove duplication by using main layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{str_replace('_','=',app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us | Hello City</title>
-</head>
-<body>
-    <p>Built with &hearts; by ELFID K@K@U.</p>
+@extends('main')
 
-    <p><a href="/">Revenir à  la page d'Accueil</a></p>
-    
-    <footer>
-        <p>&copy; Copyright {{date('Y')}}</p>
-    </footer>
-</body>
-</html>
+@section('content')
+<p>Built with &hearts; by ELFID K@K@U.</p>
+
+<p><a href="/">Revenir à  la page d'Accueil</a></p>
+
+@endsection
+
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{str_replace('_','=',app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello City</title>
-</head>
-<body>
-    <h1>Hello from Quebec </h1>
+@extends('main')
 
-    <p>It's currently {{date('h:i A')}}</p>
+@section('content')
 
-    <footer>
-        <p>&copy; Copyright {{date('Y')}} &middot;
-            <a href="/about-us" >About Us</a></p>
-    </footer>
-</body>
-</html>
+<h1>Hello from Quebec </h1>
+
+<p>It's currently {{date('h:i A')}}</p>
+@endsection
